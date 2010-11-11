cc652224a2d40b45def31d9176a469c7c6bc744c
OiNutter <willmckenzie@oinutter.co.uk>
2010-11-11 15:34:06 +0000

    Enabled escaping of special characters

--- lib/CodeGenerator.php
+++ lib/CodeGenerator.php
@@ -8,7 +8,8 @@ class CodeGenerator{
 	private static $_chars = array('A','B','C','D','E','F','G','H','J','K','L','M','N','P','Q','R','T','U','V','W','X','Y');
 	private static $_nums =  array(3,4,6,7,8,9);
 	private static $_symbols = array('!','£','$','%','^','&','*','@','?','>','<','~','#');
-	private static $_patterns = array('/X/','/x/','/9/','/\?/');
+	private static $_patterns = array('/(?<!\\\\)X/','/(?<!\\\\)x/','/(?<!\\\\)9/','/(?<!\\\\)\?/','/\\\\/');
+	private static $_matchPattern = '/(\\\\[Xx9\?])|([^\\\\])/';
 
 	public function __construct($pattern,$quantity){
 		$this->_quantity = $quantity;
@@ -41,14 +42,13 @@ class CodeGenerator{
 	//private methods//
 	///////////////////
 	private function _generateCode(){
-		$bits = str_split($this->_pattern);
-
-		foreach($bits as &$char){
-			$replacements = array(self::$_chars[rand(0,count(self::$_chars)-1)],strtolower(self::$_chars[rand(0,count(self::$_chars)-1)]),self::$_nums[rand(0,count(self::$_nums)-1)],self::$_symbols[rand(0,count(self::$_symbols)-1)]);
+		preg_match_all(self::$_matchPattern,$this->_pattern,$bits);
+		foreach($bits[0] as &$char){
+			$replacements = array(self::$_chars[rand(0,count(self::$_chars)-1)],strtolower(self::$_chars[rand(0,count(self::$_chars)-1)]),self::$_nums[rand(0,count(self::$_nums)-1)],self::$_symbols[rand(0,count(self::$_symbols)-1)],'');
 			$char = preg_replace(self::$_patterns,$replacements,$char);
 		}
 
-		return implode('',$bits);
+		return implode('',$bits[0]);
 	}
 
 }
